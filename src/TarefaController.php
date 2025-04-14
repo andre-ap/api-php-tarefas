dbbb57e9e1465f1d8b685fa26fec394810f48e26
Andre Pinto <andrepinto.cg@gmail.com>
2025-04-14 18:24:06 -0300

    add controller

---
+++ src/TarefaController.php
@@ -0,0 +1,24 @@
+<?php
+
+class TarefaController
+{
+
+    public function buscar($id)
+    {
+        echo "Buscar com {$id}";
+    }
+
+    public function listar()
+    {
+        echo 'Listar Tarefas';
+    }
+
+    public function criar(){
+        echo 'Criar Tarefa';
+    }
+
+    public function atualizar($id)
+    {
+        echo "Atualizar tarefa {$id}";
+    }
+}
