86af4680e5c456daddc19859cda5baea0d4c0f10
Andre Pinto <andrepinto.cg@gmail.com>
2025-04-14 20:34:54 -0300

    add autoload file

--- api/index.php
+++ api/index.php
@@ -1,21 +1,22 @@
 <?php
-require_once './src/TarefaController.php';
+
+require( dirname(__DIR__) . "/vendor/autoload.php");
 
 $url = $_SERVER['REQUEST_URI'];
 $metodo = $_SERVER['REQUEST_METHOD'];
 $controller = new TarefaController;
 
-if ($metodo === 'GET' && preg_match('/^\/tarefas\/([0-9]+)\/?$/i', $url, $casamentos)) {
+if ($metodo === 'GET' && preg_match('/^\/api\/tarefa\/([0-9]+)\/?$/i', $url, $casamentos)) {
     // Busca tarefa com ID
     list(, $id) = $casamentos;
     $controller->buscar($id);
-} elseif ($metodo === 'GET' && preg_match('/^\/tarefas\/?$/i', $url)) {
+} elseif ($metodo === 'GET' && preg_match('/^\/api\/tarefas\/?$/i', $url)) {
     // Listar todas as tarefas
     $controller->listar();
-} elseif ($metodo === 'POST' && preg_match('/^\/tarefas\/?$/i', $url)) {
+} elseif ($metodo === 'POST' && preg_match('/^\/api\/tarefas\/?$/i', $url)) {
     // Criar nova tarefa
     $controller->criar();
-} elseif ($metodo === 'PUT' && preg_match('/^\/tarefas\/([0-9]+)\/?$/i', $url, $casamentos)) {
+} elseif ($metodo === 'PUT' && preg_match('/^\/api\/tarefas\/([0-9]+)\/?$/i', $url, $casamentos)) {
     // Alterar uma tarefa
     list(, $id) = $casamentos;
     $controller->atualizar($id);
