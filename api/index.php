7a8c4d03cc2312a72c60830275c59012229713e0
Andre Pinto <andrepinto.cg@gmail.com>
2025-04-15 16:03:49 -0300

    add config folder

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
 
 require(dirname(__DIR__) . "/vendor/autoload.php");
-require(dirname(__DIR__) . "/src/config.php");
+require(dirname(__DIR__) . "/config/config.php");
 require(dirname(__DIR__) . "/src/router.php");
