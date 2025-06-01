3e018cc2e8312789b7667423b9a67a4c64a0257e
Andre Pinto <andrepinto.cg@gmail.com>
2025-05-31 22:28:07 -0300

    refactor

--- api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require(dirname(__DIR__) . "/vendor/autoload.php");
-require(dirname(__DIR__) . "/config/config.php");
-
-$url = $_SERVER['REQUEST_URI'];
-$method = $_SERVER['REQUEST_METHOD'];
-
-Router::rotear($url, $method, $config);
